70d259bbf7a44588125c5d943d3ea1e2e034000b
2ue <jie746635835@163.com>
2017-08-16 13:55:18 +0800

    调整拼接url的方法以及获取数据的方法

--- src/index.js
+++ src/index.js
@@ -13,24 +13,24 @@ const target = config.target.host;
 
 /*
 * @function: 获取省份数据
-* @param: url 目标地址
+* @param: {url} 目标地址
 * */
 async function getProvice(url) {
-    console.log('========getProvice start==========');
+    console.log('========getProvice1 start==========');
     const body = await getData(url);
     const $ = cheerio.load(body);
     const lists = $('tr.provincetr a');
 
     let items = [];
-    console.log('========getProvice getData succees==========');
+    console.log('========getProvice1 getData succees==========');
     for(let i = 0; i < lists.length; i++){
         console.log(i + 'i' + $(lists[i]).text());
         const $this = $(lists[i]);
         const nextUrl = $this.attr('href');
-        // const childrens = await getCity(zipUrl([2016, nextUrl],true));
-        const childrens = [];
+        const childrens = !nextUrl ? [] :  await getListData(getUrl(url,nextUrl));
         const code = nextUrl.replace(/\D/g,'');
         const value = $this.text();
+        console.log('==>getProvice1==>',code,value)
         items.push({ code, value, childrens });
     }
     return items;
@@ -38,7 +38,7 @@ async function getProvice(url) {
 
 /*
  * @function: 获取市级数据
- * @param: url 目标地址
+ * @param: {url} 目标地址
  * */
 async function getCity(url) {
 
@@ -46,16 +46,16 @@ async function getCity(url) {
     const body = await getData(url);
     const $ = cheerio.load(body);
     const lists = $('tr.citytr');
-    console.log('getCity==>','after body');
+    console.log('getCity2==>','after body');
     let items = [];
     for(let i = 0; i < lists.length; i++){
         const $this = $(lists[i]);
         const $a = $this.find('a');
         const nextUrl = $($a[0]).attr('href');
-        const childrens = await getCounty(zipUrl([2016, nextUrl],true));
-        // const childrens = [];
+        const childrens = !nextUrl ? [] : await getCounty(getUrl(url,nextUrl));
         const code = $($a[0]).text();
         const value = $($a[1]).text();
+        console.log('==>getCity2==>',code,value)
         items.push({ code, value, childrens });
     }
     console.log('getCity==>','after forloop');
@@ -64,16 +64,16 @@ async function getCity(url) {
 
 /*
  * @function: 获取区级及其区级以下数据
- * @param: url 目标地址
+ * @param: {url} 目标地址
  * */
 
 async function getCounty(url) {
-    console.log('getCounty==>',url);
+    console.log('getCounty3==>',url);
     const body = await getData(url);
     const $ = cheerio.load(body);
     const lists = $('tr.countytr');
 
-    console.log('getCounty==>','after body');
+    console.log('getCounty3==>','after body');
     let items = [];
     for(let i = 0; i < lists.length; i++){
         const $this = $(lists[i]);
@@ -81,13 +81,62 @@ async function getCounty(url) {
         const nextUrl = $($a[0]).attr('href');
         const code = $($a[0]).text();
         const value = $($a[1]).text();
+        console.log('==>getCounty3==>',code,value)
+        const childrens = !!nextUrl? [] : await getTown(getUrl(url,nextUrl));
 
-        if(typeof nextUrl === 'undefined'){
-            const childrens = await getData(nextUrl);
-            items.push({ code, value, childrens });
-        }else {
-            items.push({ code, value });
-        }
+        items.push({ code, value, childrens });
+
+    }
+    console.log('getCounty==>','after forloop');
+    return items;
+}
+
+/*
+ * @function: 获取区级及其区级以下数据
+ * @param: {url} 目标地址
+ * */
+async function getTown(url) {
+    console.log('getCounty==>',url);
+    const body = await getData(url);
+    const $ = cheerio.load(body);
+    const lists = $('tr.towntr');
+
+    console.log('getTown==>','after body');
+    let items = [];
+    for(let i = 0; i < lists.length; i++){
+        const $this = $(lists[i]);
+        const $a = $this.find('a');
+        const nextUrl = $($a[0]).attr('href');
+        const code = $($a[0]).text();
+        const value = $($a[1]).text();
+        const childrens = !nextUrl ? [] : await getVillage(getUrl(url,nextUrl));
+
+        items.push({ code, value, childrens });
+
+    }
+    console.log('getCounty==>','after forloop');
+    return items;
+}
+
+/*
+ * @function: 获取区级及其区级以下数据
+ * @param: {url} 目标地址
+ * */
+async function getVillage(url) {
+    console.log('getVillage==>',url);
+    const body = await getData(url);
+    const $ = cheerio.load(body);
+    const lists = $('tr.villagetr');
+
+    console.log('getCounty==>','after body');
+    let items = [];
+    for(let i = 0; i < lists.length; i++){
+        const $this = $(lists[i]);
+        const $td = $this.find('td');
+        const code1 = $($td[0]).text();
+        const code2 = $($td[1]).text();
+        const value = $($td[2]).text();
+        items.push({ code1, code2, value });
 
     }
     console.log('getCounty==>','after forloop');
@@ -96,36 +145,68 @@ async function getCounty(url) {
 
 /*
 * @function: 提取一个获取数据的方法
- * @param: url 目标地址
+ * @param: {url} 目标地址
 * */
 function getData(url) {
     if(!url) return [];
     return new Promise(function (resolve,reject) {
+        console.log('enter PROMISE============')
         request({ url, encoding: null }, function (error, response, body) {
+            console.log('body',body)
             if (!error && response.statusCode === 200) {
                 console.log('========request success==========');
                 //gbk 或者 gb2312 都可以
                 resolve(iconv.decode(body, 'gb2312').toString());
             }else {
+                reject(error);
                 console.log('========request fail==========');
             }
-
         });
     })
 }
 
 /*
-* @function: 拼接url
-* @params: para 需要拼接的url数组
+* @function: 获取省级以下数据
 * */
-function zipUrl (para,type) {
-    let res = target;
-    if(!para) return res + '.html';
-    if(Object.prototype.toString.call(para) !== '[object Array]') return res + '/' + para + (!type ? '.html' : '');
-    for(let i in para){
-        res += ('/' + para[i]);
-    };
-    return res + (!type ? '.html' : '');
+async function getListData(url) {
+    console.log('==entenr==>');
+    const body = await getData(url);
+    console.log('000000000')
+    const $ = cheerio.load(body);
+    console.log('1111111111111');
+    const lists = $('table[class] tbody tr').slice(1);
+    console.log('222222222222');
+    let items = [];
+    for(let i = 0; i < lists.length; i++){
+
+        console.log('==entenr foorloop==>');
+        const $this = $(lists[i]);
+        const $a = $this.find('a');
+        const $target = $a.length == 0 ? $this.find('td') : $a;
+        const nextUrl = $a.length == 0 ? '' : $($a[0]).attr('href');
+        const code = $($target[0]).text();
+        const value = $($target[1]).text();
+        console.log('==>==>',code,value);
+        const childrens = !nextUrl? [] : await getListData(getUrl(url,nextUrl));
+        items.push({ code, value, childrens });
+    }
+
+    console.log('==end foorloop==>');
+    return items;
 }
 
-module.exports = getProvice(zipUrl([2016, 'index']));
+/*
+* @function: 拼接url
+* @params: {oldUrl} 上一次的url; {newUrl} 新的url尾
+* */
+function getUrl(oldUrl, newUrl) {
+    if(!oldUrl) return target + '/2016/index.html';
+    if(!newUrl) return oldUrl;
+    const res = oldUrl.split('/');
+    res.splice(-1,1,newUrl);
+    console.log(oldUrl,'==>',res.join('/'));
+    return res.join('/');
+
+}
+
+module.exports = getProvice(getUrl());
--- src/test.js
+++ src/test.js
@@ -5,18 +5,20 @@
 const path = require('path');
 
 const http = require('http');
+const bufferhelper = require('bufferhelper');
 const request = require('request');
 const iconv = require('iconv-lite');
 const cheerio = require('cheerio');
 
 const config = require('../config');
 const target = config.target.host;
+let index = 0;
 
 /*
  * @function: 获取省份数据
  * */
-async function getProvice() {
-    const body = await getData(zipUrl([2016, 'index']));
+async function getProvice(url) {
+    const body = await getData(url);
     const $ = cheerio.load(body);
     const lists = $('tr.provincetr a');
 
@@ -24,8 +26,8 @@ async function getProvice() {
     for(let i = 0; i < lists.length; i++){
         console.log(i + 'i' + $(lists[i]).text());
         const $this = $(lists[i]);
-        const url = $this.attr('href');
-        const childrens = [];
+        const nextUrl = $this.attr('href');
+        const childrens = !url ? [] : await getListData(getUrl(url,nextUrl));
         const code = url.replace(/\D/g,'');
         const value = $this.text();
         items.push({ code, value, childrens });
@@ -33,33 +35,56 @@ async function getProvice() {
     return items;
 };
 
-
 /*
  * @function: 提取一个获取数据的方法
  * */
 function getData(url) {
     if(!url) return [];
     return new Promise(function (resolve,reject) {
-        request({ url,encoding: null }, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                const res = iconv.decode(body, 'gb2312').toString();
-                resolve(res);
-            }
+        http.get(url, function(res){
+            const buffer = new bufferhelper();
+            res.on('data', function (chunk) {
+                console.log('=============data-data-data=========');
+                // console.log(chunk);
+                buffer.concat(chunk);
+            });
+            res.on('end',function(){
+                console.log('=============end-end-end=========');
+                resolve(iconv.decode(buffer.toBuffer(),'GBK'));
+            });
         });
-        // http.get(url, function(res){
-        //     const buffer = new bufferhelper();
-        //     res.on('data', function (chunk) {
-        //         console.log('=============data-data-data=========');
-        //         // console.log(chunk);
-        //         buffer.concat(chunk);
-        //     });
-        //     res.on('end',function(){
-        //         console.log('=============end-end-end=========');
-        //         resolve(iconv.decode(buffer.toBuffer(),'GBK'));
-        //     });
-        // });
     })
 };
+
+async function getListData(url) {
+    console.log('==entenr==>');
+    const body = await getData(url);
+    console.log('000000000')
+    const $ = cheerio.load(body);
+    console.log('1111111111111');
+    const lists = $('table[class] tbody tr').slice(1);
+    console.log('222222222222');
+    let items = [];
+    for(let i = 0; i < lists.length; i++){
+
+        console.log('==entenr foorloop==>');
+        const $this = $(lists[i]);
+        const $a = $this.find('a');
+        const $target = $a.length == 0 ? $this.find('td') : $a;
+        const nextUrl = $a.length == 0 ? '' : $($a[0]).attr('href');
+        const code = $($target[0]).text();
+        const value = $($target[1]).text();
+        console.log('==>==>',code,value);
+        const childrens = (!nextUrl || index > 2) ? [] : await getListData(getUrl(url,nextUrl));
+        // const childrens = [];
+        items.push({ code, value, childrens });
+    }
+
+    index++;
+    console.log('==end foorloop==>');
+    return items;
+}
+
 /*
  * @function: 拼接url
  * @params: para 需要拼接的url数组
@@ -75,4 +100,14 @@ function zipUrl (para,type) {
     return res + (!type ? '.html' : '');
 }
 
-module.exports = getProvice();
+function getUrl(oldUrl, newUrl) {
+    if(!oldUrl) return target + '/2016/index.html';
+    if(!newUrl) return oldUrl;
+    const res = oldUrl.split('/');
+    res.splice(-1,1,newUrl);
+    console.log(oldUrl,'==>',res.join('/'));
+    return res.join('/');
+
+}
+
+module.exports = getProvice(getUrl());
